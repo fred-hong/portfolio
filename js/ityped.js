9a55aab00479b5b20a39b7eb77a5b44ab7b42f68
hong-fred <77351665+hong-fred@users.noreply.github.com>
2023-03-19 15:27:37 -0400

    First Commit

---
+++ js/ityped.js
@@ -0,0 +1,6 @@
+ityped.init(document.querySelector("#ityped"), {
+    strings: ['Full-Stack Developer', 'Software Engineer', 'Database Administrator'],
+    showCursor: true,
+    typeSpeed: 80,
+    backSpeed: 70
+})
